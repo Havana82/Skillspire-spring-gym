07d47873ebb30ae1dfd8296c1055087a32d0e1ab
halimAremu <halimat3000@gmail.com>
2024-11-14 03:31:07 -0600

    update

--- src/main/java/com/goldsgym/golds_gym/dto/UserDto.java
+++ src/main/java/com/goldsgym/golds_gym/dto/UserDto.java
@@ -11,6 +11,6 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String address;
-    private Gym gymId;
+    private int gymId;
 }
 //
--- src/main/java/com/goldsgym/golds_gym/mapper/UserMapper.java
+++ src/main/java/com/goldsgym/golds_gym/mapper/UserMapper.java
@@ -1,19 +1,27 @@
 package com.goldsgym.golds_gym.mapper;
 
 import com.goldsgym.golds_gym.dto.UserDto;
+import com.goldsgym.golds_gym.models.Gym;
 import com.goldsgym.golds_gym.models.User;
+import com.goldsgym.golds_gym.services.GymService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
+    @Autowired
+    private GymService gymService;
+    @Autowired
+    private GymMapper gymMapper;
     // Convert DTO object into a Entity object
     public User mapToUser(UserDto userDto){
+        Gym gymFromDb = gymMapper.mapToGym(gymService.getGymById(userDto.getGymId()));
         User user = new User(
                 userDto.getId(),
                 userDto.getFirstName(),
                 userDto.getLastName(),
                 userDto.getAddress(),
-                userDto.getGymId()
+                gymFromDb
         );
         return user;
     }
@@ -25,7 +33,7 @@ public class UserMapper {
                 user.getFirstName(),
                 user.getLastName(),
                 user.getAddress(),
-                user.getGym_id()
+                user.getGym_id().getId()
         );
         return userDto;
     }
